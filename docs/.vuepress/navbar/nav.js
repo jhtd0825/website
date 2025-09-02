f11c6d40e65fa4da2d1edc9edd7d62f91b330ffb
jhtd0825 <1412907243@qq.com>
2025-09-02 09:21:34 +0800

    内容修改

---
+++ docs/.vuepress/navbar/nav.js
@@ -0,0 +1,131 @@
+// Java导航栏
+module.exports =  [
+  { text: '首页', link: '/' },
+  { text: '公司简介', link: '/pages/f93792/' },
+
+{
+  text: '产品分类',
+  items: [
+    {
+      text: '图文鉴赏',
+      items: [
+        { text: '电视柜', link: '/pages/78kh78/' },
+        { text: '玄关柜', link: '/pages/1b128k/' },
+        { text: '餐边柜', link: '/pages/1b12lp/'},
+        { text: '走廊柜', link: '/pages/785pop/'},
+        { text: '衣帽柜', link: '/pages/1b87em/'},
+        { text: '阳台柜', link: '/pages/520kkm/'},
+        { text: '飘窗柜', link: '/pages/889qwe/'},
+        { text: '收纳柜', link: '/pages/74kj5g/'},
+        { text: '床头柜', link: '/pages/556qwe/'},
+        { text: '酒柜', link: '/pages/2k12ed/'},
+        { text: '鞋柜', link: '/pages/854hgt/'},
+      ]
+    },
+    {
+      text: '我的文档',
+      items: [
+        { text: '测试', link: '/private/wedding/' },
+        { text: '关于', link: 'https://docs.dakecn.top/' },
+      ]
+    },
+  ]
+},
+
+
+
+ { text: '资讯中心', 
+  items:[
+    { text: '旅游团建', link: '/pages/9cc27d/' },
+    { text: '工作会议', link: '/pages/6343a7/' },
+    { text: '考察交流', link: '/pages/1d82c3/' },
+    { text: '产品质量', link: '/pages/1d82c3/' },
+    { text: '苔花家居', link: '/pages/3dda65/' },
+    { text: '校园招聘', link: '/pages/2dda55/' },
+
+    {
+      text: '我的科普',
+      items: [
+        
+        { text: '混知', link: '/pages/hunzhi/' },
+       
+      ]
+    }
+  
+
+  ]
+  },
+
+  /** 注释代码
+  { text: '程序设计',
+    items:[
+      {
+        text: '前端案例',
+        items: [
+          
+          { text: 'Demo_One', link: '/pages/4f8616/' },
+          { text: 'Demo_Two', link: '/pages/becc93/' },
+         
+        ]
+      },    
+    ]
+
+  },
+*/
+
+ //{ text: '产品中心', link: '/pages/1b12ed/' },
+ { text: '招商加盟', link: '/pages/db78e2/' },
+ { text: '联系我们', link: '/pages/5d571c/' },
+ { text: '导航站', link: '/pages/daohang/' },
+ { text: '留言区', link: '/message-area/' }
+
+
+
+  /*
+   {
+  text: '关于我们',
+items: [
+  { text: '轮子工具', link: '/whells-use/' },
+  { text: '项目工程', link: '/projects/' },
+  { text: '友情链接', link: '/friends/' }
+]
+},
+  {
+    text: '指南', link: '/pages/a2f161/', items: [
+      { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+      { text: '介绍', link: '/pages/a2f161/' },
+      { text: '快速上手', link: '/pages/793dcb/' },
+      { text: '目录结构', link: '/pages/2f674a/' },
+      { text: '核心配置和约定', link: '/pages/33d574/' },
+      { text: '自动生成front matter', link: '/pages/088c16/' },
+      { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
+      { text: 'Markdown 中使用组件', link: '/pages/197691/' },
+      {
+        text: '相关文章', items: [
+          { text: '使目录栏支持h2~h6标题', link: '/pages/8dfab5/' },
+          { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
+          { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+          { text: '部署', link: '/pages/0fc1d2/' },
+          { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+          { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+          { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+        ]
+      }
+    ]
+  },
+  
+  {
+    text: '配置', link: '/pages/a20ce8/', items: [
+      { text: '主题配置', link: '/pages/a20ce8/' },
+      { text: '首页配置', link: '/pages/f14bdb/' },
+      { text: 'front matter配置', link: '/pages/3216b0/' },
+      { text: '目录页配置', link: '/pages/54651a/' },
+      { text: '添加摘要', link: '/pages/1cc523/' },
+      { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+      { text: '评论栏', link: '/pages/ce175c/' },
+    ],
+    
+  },
+
+  */
+]
